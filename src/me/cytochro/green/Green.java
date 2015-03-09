dd4013b5bfb65c691563fdf7e212de7b419d5d86
Samuel Edwin Ward <seward@cytochro.me>
2015-03-08 18:49:09 -0700

    The start of an interpreter!

---
+++ src/me/cytochro/green/Green.java
@@ -0,0 +1,37 @@
+package me.cytochro.green;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+
+import me.cytochro.zson.EOF;
+import me.cytochro.zson.Nil;
+import me.cytochro.zson.Objet;
+import me.cytochro.zson.Symbol;
+import me.cytochro.zson.ZSON;
+
+public class Green {
+    public static void main(String [] args) throws IOException {
+        Green me = new Green();
+        try (InputStreamReader isr = new InputStreamReader(System.in);
+             BufferedReader in = new BufferedReader(isr);
+             PrintWriter out = new PrintWriter(System.out)) {
+            for (Objet o = ZSON.read(in); !(o instanceof EOF); o = ZSON.read(in)) {
+                ZSON.write(me.eval(o), out);
+                out.flush();
+            }
+        }
+    }
+
+    public Objet eval(Objet expression) {
+        if (expression instanceof Nil) {
+            return expression;
+        } else if (expression instanceof Symbol) {
+            return new Unbound((Symbol) expression);
+        } else {
+            throw new UnsupportedOperationException("TODO!!");
+        }
+    }
+}
---
+++ src/me/cytochro/green/Unbound.java
@@ -0,0 +1,35 @@
+package me.cytochro.green;
+
+import java.io.IOError;
+import java.io.IOException;
+
+import me.cytochro.zson.Objet;
+import me.cytochro.zson.Symbol;
+
+/**
+ * Represents an unbound variable.
+ */
+public class Unbound implements Objet {
+    public Unbound(Symbol name) {
+        this.symbol = name;
+    }
+
+    private final Symbol symbol;
+
+    public String toString() {
+        try {
+            return toString(new StringBuffer()).toString();
+        } catch (IOException ioe) {
+            throw new IOError(ioe);
+        }
+    }
+
+    public Appendable toString(Appendable buf) throws IOException {
+        return buf
+            .append("#<")
+            .append(super.toString())
+            .append("<")
+            .append(symbol.toString())
+            .append(">>");
+    }
+}
