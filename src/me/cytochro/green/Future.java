6b08ca351a8db1677fcc137a1f0c0c3fa30e355d
Samuel Edwin Ward <seward@cytochro.me>
2015-03-30 18:28:19 -0700

    Add Future convenience method to form a Future from an Objet

--- src/me/cytochro/green/Future.java
+++ src/me/cytochro/green/Future.java
@@ -9,4 +9,8 @@ public interface Future extends java.util.function.Supplier<Objet> {
     public default Set<Future> getDependencies() {
         return Collections.EMPTY_SET;
     }
+
+    public static Future of(Objet o) {
+        return () -> o;
+    }
 }
