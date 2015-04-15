e7ac07bf77b90ef1b3e75761a9200d6ed25be31c
Samuel Edwin Ward <seward@cytochro.me>
2015-04-14 21:28:05 -0700

    factor lambda guts into their own class for reuse

--- src/me/cytochro/green/special/operator/Lambda.java
+++ src/me/cytochro/green/special/operator/Lambda.java
@@ -21,6 +21,52 @@ import me.cytochro.green.exception.ArityException;
 import me.cytochro.green.exception.Unbound;
 
 public class Lambda implements T, SpecialOperator {
+    public static class Guts implements Function {
+        protected final T[] parameters;
+        protected final List body;
+        protected final LexicalEnvironment outer;
+        protected final Green runtime;
+
+        public Guts(Cons guts, LexicalEnvironment outer, Green runtime) {
+            final T listO = guts.getCar();
+            assert (listO instanceof List);
+            final List list = (List) listO;
+            parameters = list.toArray();
+            final T bodyO = guts.getCdr();
+            assert (bodyO instanceof List);
+            body = (List) bodyO;
+            this.outer = outer;
+            this.runtime = runtime;
+        }
+
+        public Future apply(Future [] arguments) {
+            if (arguments.length != parameters.length) {
+                return () ->
+                    new ArityException(this,
+                                       arguments.length,
+                                       parameters.length,
+                                       parameters.length);
+            }
+
+            final ImmutableMap.Builder<Symbol, Future> b =
+                new ImmutableMap.Builder<>();
+            for (int i = 0; i < arguments.length; i++) {
+                Symbol name = (Symbol) parameters[i]; // TODO check type
+                Future value = arguments[i];
+                if (value.get() instanceof Exception) {
+                    return value;
+                }
+                b.put((Symbol) name, arguments[i]);
+            }
+
+            final LexicalEnvironment inner =
+                new LexicalEnvironment(outer, b.build());
+
+            T expression = body.getCar();
+            return runtime.evalForFuture(expression, inner);
+        }
+    }
+    
     @Override
     public Future eval(T expression, LexicalEnvironment lexenv) {
         return () -> {
@@ -30,42 +76,8 @@ public class Lambda implements T, SpecialOperator {
             assert (LAMBDA.equals(lambda));
             final T restO = expr.getCdr();
             assert (restO instanceof Cons);
-            final Cons rest = (Cons) restO;
-            final T parmsO = rest.getCar();
-            assert (parmsO instanceof List);
-            final List parmsList = (List) parmsO;
-            final T[] parameters = parmsList.toArray();
-            final T bodyO = rest.getCdr();
-            assert (bodyO instanceof List);
-            final List body = (List) bodyO;
-            return new Function() {
-                public Future apply(Future [] arguments) {
-                    if (arguments.length != parameters.length) {
-                        return () ->
-                            new ArityException(this,
-                                               arguments.length,
-                                               parameters.length,
-                                               parameters.length);
-                    }
-
-                    final ImmutableMap.Builder<Symbol, Future> b =
-                        new ImmutableMap.Builder<>();
-                    for (int i = 0; i < arguments.length; i++) {
-                        Symbol name = (Symbol) parameters[i]; // TODO check type
-                        Future value = arguments[i];
-                        if (value.get() instanceof Exception) {
-                            return value;
-                        }
-                        b.put((Symbol) name, arguments[i]);
-                    }
-
-                    final LexicalEnvironment inner =
-                        new LexicalEnvironment(lexenv, b.build());
-                    
-                    T expression = body.getCar();
-                    return runtime.evalForFuture(expression, inner);
-                }
-            };
+            final Cons guts = (Cons) restO;
+            return new Guts(guts, lexenv, runtime);
         };
     }
 
